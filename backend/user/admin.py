f7126338cb1a03e4f7f571f0344eb0c940653358
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-04-20 10:58:18 -0400

    add admin for custom user model.

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 # Register your models here.
 
-admin.site.register(CustomUser)
+UserAdmin.add_fieldsets[0][1]['fields'] = (
+    "first_name",
+    "last_name",
+    "username",
+    'phone_number',
+    "password1",
+    "password2" ,
+    "organizer"   
+)
+UserAdmin.fieldsets[1][1]['fields'] = (
+    "first_name",
+    "last_name",
+    "email",
+    "phone_number",
+)
+UserAdmin.fieldsets[2][1]['fields'] = (
+    "is_active",
+    "is_staff",
+    "is_superuser",
+    "organizer",
+    "groups",
+    "user_permissions",
+)
+UserAdmin.list_display = ('username', 'get_full_name', 'email', 'is_superuser', 'is_staff', 'is_active')
+UserAdmin.list_filter += ("organizer",)
+admin.site.register(CustomUser, UserAdmin)
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
-    OPERATOR = 'OPT'
-    PRESENTER = 'PRS'
-    GUEST = 'GUT'
-    USER_ROLE_CHOICES = [
-        (OPERATOR, 'اپراتور'),
-        (PRESENTER, 'ارائه دهنده'),
-        (GUEST, 'مهمان')
-    ]
+    # OPERATOR = 'OPT'
+    # PRESENTER = 'PRS'
+    # GUEST = 'GUT'
+    # USER_ROLE_CHOICES = [
+    #     (OPERATOR, 'اپراتور'),
+    #     (PRESENTER, 'ارائه دهنده'),
+    #     (GUEST, 'مهمان')
+    # ]
 
     email = models.EmailField(max_length=200, unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
-    role = models.CharField(choices=USER_ROLE_CHOICES, max_length=3, default=GUEST)
+    # role = models.CharField(choices=USER_ROLE_CHOICES, max_length=3, default=GUEST)
     organizer = models.BooleanField(default=False)
-    membership_date = models.DateTimeField(auto_now_add=True)
+    membership_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def shown_name(self):
         return self.first_name + " " + self.last_name
@@ -37,3 +38,4 @@ class CustomUser(AbstractUser):
 
     def files_volume(self):
         pass
+
